7e579678cafc543923d05fd5608cbc067bf8958d
trisetiobr <trisetiobr@gmail.com>
2024-08-23 05:27:54 +0700

    edit

--- blorgh/lib/blorgh/configuration.rb
+++ blorgh/lib/blorgh/configuration.rb
@@ -1,18 +1,10 @@
 module Blorgh
   class Configuration
-    attr_reader :name, :email
+    attr_accessor :name, :email
 
     def initialize
       @name = 'Lorem'
       @email = 'lorem@ipsum.com'
-    end
-
-    def define_email(email)
-      @email = email
-    end
-
-    def define_name(name)
-      @name = name
     end
   end
 end
