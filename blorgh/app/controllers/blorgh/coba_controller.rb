f6970c31174d895284b279c303605972d3c875cb
trisetiobr <trisetiobr@gmail.com>
2024-08-23 05:37:04 +0700

    add tag v.0.1.3

--- blorgh/app/controllers/blorgh/coba_controller.rb
+++ blorgh/app/controllers/blorgh/coba_controller.rb
@@ -1,7 +1,7 @@
 module Blorgh
   class CobaController < ApplicationController
     def index
-      render json: { message: "hello", name: Blorgh.configuration.name, email: Blorgh.configuration.email }, status: :ok
+      render json: { message: "hello version v1.0.3", name: Blorgh.configuration.name, email: Blorgh.configuration.email }, status: :ok
     end
   end
 end
--- blorgh/lib/blorgh/version.rb
+++ blorgh/lib/blorgh/version.rb
@@ -1,3 +1,3 @@
 module Blorgh
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
